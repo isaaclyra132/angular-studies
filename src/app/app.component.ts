a3d007e6c1e00c491af6382786e4cf3c489ced01
Isaac de Lyra Junior <isaac.lyra.junior@gmail.com>
2022-07-28 15:12:50 -0300

    directives module finished :tada:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   count: number = 0;
+  nome: string = "Isaac de Lyra";
+  text: string;
+
+  pessoas = [
+    {
+      nome: "Isaac",
+      sobrenome: "Lyra"
+    },
+    {
+      nome: "Julia",
+      sobrenome: "Dantas"
+    },
+    {
+      nome: "Douglas",
+      sobrenome: "Dantas"
+    },
+    {
+      nome: "Jefferson",
+      sobrenome: "Silva"
+    }
+  ];
 
   constructor() {
 
   }
 
   ngOnInit() {
+    console.log(this.pessoas)
     let interval = setInterval(()=> {
       this.count++;
       if(this.count == 10) {
         clearInterval(interval);
       }
     }, 1000)
+  }
+
+  clicou(nome: string): void {
+    console.log('Clicou em mim', nome)
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MarcaTextoDirective } from './directives/marca-texto.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarcaTextoDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
